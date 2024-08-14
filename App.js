f486f4b28383a405b9a36d63f2cd2137d6d161d3
Ayomide Akintan <emzyakints2005@gmail.com>
2024-08-14 15:34:41 +0100

    working on the calculator

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import { numbers } from './constants/calculatorStructure';
+import Button from './components/button';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text className="text-blue-900 text-3xl">Open up App.js to start working on your app!</Text>
+      <View className="bg-black">
+        <StatusBar style="light" />
+        <View className="flex h-full w-screen">
+          <View className="w-full h-1/3"></View>
+          <View className="w-full flex-1 grid">
+            <View className="h-fit w-full bg-red-400">
+              {numbers.map((row, index) => (
+                <Button key={index} text={row.text} />
+              ))}
+            </View>
+            <View className=""></View>
+          </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
